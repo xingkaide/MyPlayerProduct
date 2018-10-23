e4bb0b9442b7956d1653369f35b9409a0a498556
xingkai <xingkai@fimi.com>
2018-10-23 19:31:46 +0800

    first commit

---
+++ MyPlayerProduct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlayerProduct
+//
+//  Created by admin on 2018/10/23.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
