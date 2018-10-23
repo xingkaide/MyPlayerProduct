2a6cf142896814a3c32b50b63152aed1cd33fd5f
xingkai <xingkai@fimi.com>
2018-10-23 19:47:24 +0800

    add commkit

---
+++ MyPlayerProduct/PlayerView.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerView.swift
+//  MyPlayerProduct
+//
+//  Created by admin on 2018/10/23.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+}
