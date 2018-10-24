6e5659a05cc5adbaed8f7a97dde0d4e4ce902e64
xingkai <xingkai@fimi.com>
2018-10-24 10:15:23 +0800

    再次增加一个测试文件

---
+++ MyPlayerProduct/PlayerView.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerView.swift
+//  MyPlayerProduct
+//
+//  Created by admin on 2018/10/24.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: NSObject {
+
+}
